2e843aa76bb594e7558ec0a58188b4c02e2d5d32
Erin Nielsen <erin.nielsen@enterprisedb.com>
2024-05-06 16:49:22 -0600

    This wrapper will replace the schema tag with all schemas within the database specified.  Usage is ./wrapper.sh <Original YML> <Updated YML>

---
+++ wrapper/wrapper.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Function to execute PostgreSQL query
+getschemas_query()
+{
+   local database_name="$1"
+   psql -t -c "select schema_name from information_schema.schemata where catalog_name = '$database_name' and schema_name <> 'public';"
+}
+
+# Function to replace "schemas" tag in YAML with comma-delimited schema list
+replace_schemas_tag() {
+
+    local yml_file=$1
+    local database=$2
+
+    # get the list of schemas for this database
+    local query_results=$(getschemas_query "$database")
+    echo $query_results
+
+    # Get results as a comma delimited string
+    local schema_list=$(echo "$query_results" | tr '\n' ',' | sed 's/,$//')
+
+    schema_list="${schema_list// /}"
+
+    #output list of schemas found.
+    echo "Schemas found: '$schema_list'"
+
+    local updated_yaml=$(sed "s/schema:.*/schema: \"$schema_list\"/g" $yml_file)
+
+    echo "$updated_yaml" > new_ldap2pg.yml
+}
+
+# Main script starts here
+
+# Check if YAML file is provided as argument
+if [ $# -ne 2 ]; then
+    echo "Missing YML file and databases to update.  Usage: $0 <input_yaml_file> <pg_database_name>"
+    exit 1
+fi
+
+replace_schemas_tag "$1" "$2"
+echo "Done!"
